4480ed3d6412df65c481a715a4b62f5c19269faf
Mary Touch <marytouch@Marys-MBP.fios-router.home>
2018-01-08 16:44:10 -0500

    010818 commit

--- src/com/helper/DatabaseHelper.java
+++ src/com/helper/DatabaseHelper.java
@@ -1,7 +1,10 @@
 package com.helper;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 
 // mySQL helper functions
@@ -22,44 +25,46 @@ public class DatabaseHelper {
 		this.table = TableName;
 	}
 
+	
+	// add a row into the database
 	private void prepareTheDB(String keyword1, String keyword2, int freq, String query) {
 		try {
-			java.sql.PreparedStatement preparedStmt = conn.prepareStatement(query);
-
+			PreparedStatement preparedStmt = (PreparedStatement) conn.prepareStatement(query);
 			preparedStmt.setString (1, keyword1);
 			preparedStmt.setString (2, keyword2);
 			preparedStmt.setInt (3, freq);
-
 			preparedStmt.execute();
+			preparedStmt.close();
 			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	
 	// insert data into database
 	public void insert(String keyword1, String keyword2, int freq) { // query:(id, key1, key2, freq) where id is autoincrement
 		String query = " insert into WordsTable (Key1, Key2, Frequency)"
 				+ " values (?, ?, ?)";
 		prepareTheDB(keyword1, keyword2, freq, query);
 	}
+	
 
 	// update frequency of occurrence
 	public void updateFrequency(String keyword1, String keyword2, int num) {
-		int tempID = 7;
+		int tempID = getID(keyword1, keyword2);
 		
-		// call getID(keyword1, keyword2);
+		if(tempID == -1) {
+			System.out.println("ERROR in getting ID.");
+//			throw new NoIDfoundException();
+		}
 
-		String keyPair = String.format("%s.key1 = '%s' and "
-				+ "%s.key2 = '%s' and %s.id = %d", 
-				this.table, keyword1, this.table, keyword2, this.table, tempID);
-//		System.out.println("keyPair: "+keyPair);
-		String updateQuery = String.format("UPDATE %s "
-				+ " SET Frequency = %d WHERE "
-				+ keyPair, 
-				this.table, num);
+		String updateQuery = String.format("UPDATE %s SET %s.Frequency = ? "
+				+ "WHERE id = %d", this.table, this.table, tempID);
+		
 		try {
-			java.sql.PreparedStatement preparedStmt = conn.prepareStatement(updateQuery);
+			PreparedStatement preparedStmt = (PreparedStatement) conn.prepareStatement(updateQuery);
+			preparedStmt.setInt(1, num);
 			preparedStmt.executeUpdate();
 			conn.close();
 		} catch (Exception e) {
@@ -67,20 +72,94 @@ public class DatabaseHelper {
 		}
 	}
 
-	private int getFrequency() {
+	
+	// get frequency of a pair of words
+	public int getFrequency(String keyword1, String keyword2) {
 		int freq = -1;
+		ResultSet resultSet = null;
 		
+		// from the getID function, use it to get the frequency
+		String query = String.format("SELECT %s.Frequency FROM %s "
+				+ "WHERE %s.Key1 = '%s' and %s.Key2 = '%s'", 
+				this.table, this.table, this.table, keyword1, this.table, keyword2);
+		try {
+			PreparedStatement preparedStmt = (PreparedStatement) conn.prepareStatement(query);
+			resultSet = preparedStmt.executeQuery();
+			while(resultSet.next()) {
+				freq = resultSet.getInt(1);
+			}
+			resultSet.close();
+			preparedStmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 		return freq;
 	}
 	
+	
+	// get the id of the row or words pair
 	private int getID(String keyword1, String keyword2) {
 		int tempID = -1;
+		ResultSet resultSet = null;
 
+		String query = String.format("SELECT %s.id FROM %s WHERE %s.Key1 = '%s' and " + 
+				"%s.Key2 = '%s'", this.table, this.table, this.table, keyword1, this.table, keyword2);
+		try {
+			PreparedStatement preparedStmt = (PreparedStatement) conn.prepareStatement(query);
+			resultSet = preparedStmt.executeQuery();
+			while(resultSet.next()) {
+				tempID = resultSet.getInt(1);
+			}
+			resultSet.close();
+			preparedStmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 		return tempID;
 	}
+	
+	
+	// given key1, return a list of all its related key2's
+	public List<String> getRelatedWords(String keyword) {
+		ResultSet resultSet = null;
+		List<String> relatedList = new ArrayList();
+		
+		String query = String.format("SELECT * FROM %s WHERE %s.Key1 = '%s'", this.table, this.table, keyword);
+		
+		try {
+			PreparedStatement preparedStmt = (PreparedStatement) conn.prepareStatement(query);
+			resultSet = preparedStmt.executeQuery();
+			while(resultSet.next()) {
+				int id = resultSet.getInt(1);
+				String k1 = resultSet.getString(2);
+				String k2 = resultSet.getString(3);
+				int freq = resultSet.getInt(4);
+				relatedList.add(String.format("%d  %s  %s  %d", id, k1, k2, freq));
+			}
+			resultSet.close();
+			preparedStmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return relatedList;
+	}
 
-	// delete from database
-	public void delete() {
-
+	
+	// delete a row from database using words 
+	public void deleteByWordsPair(String keyword1, String keyword2) {
+		deleteByID(getID(keyword1, keyword2));
+	}
+	
+	
+	// delete a row from database using id
+	public void deleteByID(int id) {
+		String query = String.format("DELETE FROM %s where %s.id = %d", this.table, this.table, id);
+		try {
+			PreparedStatement preparedStmt = (PreparedStatement) conn.prepareStatement(query);
+			preparedStmt.execute();
+			preparedStmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 	}
 }
--- src/com/test/TestApp.java
+++ src/com/test/TestApp.java
@@ -1,29 +1,29 @@
 package com.test;
 
-import java.sql.Connection;
+//import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
-
-import com.database.MySqlDB;
 import com.helper.DatabaseHelper;
-import com.ner.NERparser;
-import com.nlp.NLPparser;
+
+import com.mysql.jdbc.Connection;
 
 public class TestApp {
 	public static void main(String[] args) throws SQLException {
-		NLPparser nlpParser = new NLPparser();
-		List<String> nlpResults = nlpParser.getPOSfromSentence("Mary is the owner of MaryTheBest Corp.", "NNP");
-
+//		NLPparser nlpParser = new NLPparser();
+//		List<String> nlpResults = nlpParser.getPOSfromSentence("Mary is the owner of MaryTheBest Corp.", "NNP");
+//		System.out.println(nlpResults);
+		
 		// create a mysql database connection
-		MySqlDB myDB = new MySqlDB();
 		String myUrl = "jdbc:mysql://localhost:3306/WordsRelationSql?autoReconnect=true&useSSL=false";
-		Connection myConn = DriverManager.getConnection(myUrl, "root", "admin");
+		Connection myConn = (Connection) DriverManager.getConnection(myUrl, "root", "admin");
 		
 //		myDB.insertToDatabase(myUrl, myConn, nlpResults);
 		
 		DatabaseHelper helper = new DatabaseHelper(myUrl, myConn, "WordsTable");
-//		helper.insert("Thiago", "isOkay", 3);
-		helper.updateFrequency("Mary", "TheBest", 6);
+//		helper.insert("Mary", "TheBest3", 3);
+//		helper.updateFrequency("Mary", "TheBest", 1);
+//		helper.getRelatedWords("Mary");
+//		helper.getFrequency("Mary", "TheBest3");
+//		helper.deleteByWordsPair("Mary", "TheBest2");
 	}
 }
